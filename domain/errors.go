d0aa76a821b0fb9f2f66453cb4a5a94366411903
Wagner Domingos <wagnerdomingos@gmail.com>
2025-10-24 20:30:44 -0300

    Adding missing error, when user try to send a message for himself

--- domain/errors.go
+++ domain/errors.go
@@ -2,12 +2,13 @@ package domain
 
 // Application errors
 var (
-	ErrUserNotFound    = &AppError{"user not found", 404}
-	ErrChatNotFound    = &AppError{"chat not found", 404}
-	ErrMessageNotFound = &AppError{"message not found", 404}
-	ErrUsernameExists  = &AppError{"username already exists", 409}
-	ErrInvalidUser     = &AppError{"invalid user", 400}
-	ErrEmptyMessage    = &AppError{"message content cannot be empty", 400}
+	ErrUserNotFound      = &AppError{"user not found", 404}
+	ErrChatNotFound      = &AppError{"chat not found", 404}
+	ErrMessageNotFound   = &AppError{"message not found", 404}
+	ErrUsernameExists    = &AppError{"username already exists", 409}
+	ErrInvalidUser       = &AppError{"invalid user", 400}
+	ErrCannotMessageSelf = &AppError{"cannot message yourself", 400}
+	ErrEmptyMessage      = &AppError{"message content cannot be empty", 400}
 )
 
 // AppError represents an application error with HTTP status code
